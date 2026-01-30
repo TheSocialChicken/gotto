05ce23bb3dfccf9158216d5112de85d72adf9ddd
Filip Vranesevic <filip.vranesevic@basiq.io>
2026-01-30 11:09:08 +0100

    remote package

---
+++ remote/remote.go
@@ -0,0 +1,90 @@
+package remote
+
+import (
+	"errors"
+	"time"
+
+	ninja "github.com/HattoriHanzo031/otto_ninja"
+	"github.com/HattoriHanzo031/otto_ninja/buzzer"
+)
+
+type Op int
+
+const (
+	OpSetMode Op = iota
+	OpWalk
+	OpRoll
+	OpTiltRight
+	OpTiltLeft
+	OpHome
+	OpRightLegSpin
+	OpLeftLegSpin
+	OpBuzzerTone
+	OpWave
+	OpCustom
+)
+
+var (
+	ErrUnknownCommand = errors.New("ninja: invalid command")
+)
+
+type Command struct {
+	Op   Op
+	Args [2]int
+}
+
+func (c *Command) Execute(n *ninja.Ninja) error {
+	switch c.Op {
+	case OpSetMode:
+		switch c.Args[0] {
+		case 0:
+			return n.Mode(ninja.ModeWalk)
+		case 1:
+			return n.Mode(ninja.ModeRoll)
+		}
+	case OpHome:
+		return n.Home()
+	case OpTiltLeft:
+		switch c.Args[0] {
+		case 0:
+			return n.Tilt(ninja.TiltReturnFromLeft)
+		case 1:
+			return n.Tilt(ninja.TiltLeft)
+		}
+	case OpTiltRight:
+		switch c.Args[0] {
+		case 0:
+			return n.Tilt(ninja.TiltReturnFromRight)
+		case 1:
+			return n.Tilt(ninja.TiltRight)
+		}
+	case OpLeftLegSpin:
+		if c.Args[0] == 1 {
+			return n.StartLeftSpin(30)
+		} else {
+			return n.StopLeftSpin()
+		}
+	case OpRightLegSpin:
+		if c.Args[0] == 1 {
+			return n.StartRightSpin(30)
+		} else {
+			return n.StopRightSpin()
+		}
+	case OpWalk:
+		return n.Walk(c.Args[0])
+	case OpRoll:
+		return n.Roll(c.Args[1], c.Args[0])
+	case OpBuzzerTone:
+		return n.BuzzerTone(buzzer.Note{
+			Period:   buzzer.NotePeriod(c.Args[0]),
+			Duration: time.Duration(c.Args[1]) * time.Millisecond,
+		})
+	case OpWave:
+		return n.Wave()
+	case OpCustom:
+		return n.ExecuteCustomCommand(c.Args[0])
+	default:
+		return ErrUnknownCommand
+	}
+	return nil
+}
